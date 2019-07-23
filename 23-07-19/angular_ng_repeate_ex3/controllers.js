a11273a18075ab12b3b1f8a84830d05bc171b513
yoave11 <yoave11@yahoo.com>
2019-07-23 16:06:51 +0300

    order by

--- 23-07-19/angular_ng_repeate_ex3/controllers.js
+++ 23-07-19/angular_ng_repeate_ex3/controllers.js
@@ -5,22 +5,30 @@ module.controller("parentCtrl", ParentCtrl)
 // DI dependency injection - IOC
 function ParentCtrl($scope, $rootScope) {
 
-    $scope.devices = [
+    $rootScope.devices = [
         new Mobile({ model: "IPhone X", color: "black", price: 3500 }),
         new Mobile({ model: "Xiaomi", color: "green", price: 1000 }),
         new Mobile({ model: "Samsung Galaxy 11+", color: "black", price: 5500 }),
         new Mobile({ model: "LG V60", color: "White", price: 7500 })
     ]
 
-    $scope.remove = (index) => $scope.devices.splice(index, 1)
+    $scope.remove = (index) => $rootScope.devices.splice(index, 1)
 
-    $scope.add = ({ model, color, price }) => $scope.devices.push(new Mobile({ model, color, price }))
+    $scope.add = ({ model, color, price }) => $rootScope.devices.push(new Mobile({ model, color, price }))
     $scope.update = ({ model, color, price }, index) => {
-        if (!index || index > $scope.devices.length - 1) {
+        if (!index || index > $rootScope.devices.length - 1) {
             alert('out of bounds!')
             return
         }
-        $scope.devices[index] = new Mobile({ model, color, price })
+        $rootScope.devices[index] = new Mobile({ model, color, price })
     }
 
+    $scope.select = index => {
+        console.log(index)
+        $scope.selected = index
+    }
 }
+
+module.controller("footerCtrl", ($scope, $rootScope) => {
+
+})
